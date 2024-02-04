51e67047eb60120c03e3647b49446f434c020971
Kintong Cheung <cheungkintong3000@gmail.com>
2024-02-04 21:13:05 +0800

    backup

--- blog-fronted/nuxt.config.ts
+++ blog-fronted/nuxt.config.ts
@@ -1,27 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // app: {
-    // head: {
-    //   "meta": [
-    //     {
-    //       "name": "viewport",
-    //       "content": "width=device-width, initial-scale=1"
-    //     },
-    //     {
-    //       "charset": "utf-8"
-    //     }
-    //   ],
-    //   "link": [{rel:"stylesheet",href:"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css"}],
-    //   "style": [],
-    //   "script": [{src:"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"},{src:"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"}],
-    //   "noscript": []
-    // }},
+
     // ssr: false,
     devtools: {enabled: true},
     css: [
         '~/assets/styles/main.css',
         '~/assets/styles/rainbow.css',
         '~/assets/styles/markdown.css',
+        '~/assets/styles/prose.css',
         '@unocss/reset/tailwind.css'
     ],
 
