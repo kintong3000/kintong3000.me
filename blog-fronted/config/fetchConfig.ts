33512724ab42865adfec248c06ba331c4b8f6082
Kintong Cheung <cheungkintong3000@gmail.com>
2024-02-08 21:57:56 +0800

    test

--- blog-fronted/config/fetchConfig.ts
+++ blog-fronted/config/fetchConfig.ts
@@ -2,7 +2,7 @@ const isProd:boolean = process.env.NODE_ENV === 'production'
 
 const devUrl:string = 'https://dev.usemock.com/65bdf10d6309cc7a3772327b'
 
-const prodUrl:string = ''
+const prodUrl:string = 'https://dev.usemock.com/65bdf10d6309cc7a3772327b'
 
 const config:{isProd:boolean,api:string} = {
     isProd,
