dfaae14967695afec7b002f5d602072b97a7fc0c
Kintong Cheung <cheungkintong3000@gmail.com>
2024-03-06 16:18:06 +0800

    login view

--- blog-admin/src/main.js
+++ blog-admin/src/main.js
@@ -3,6 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:uno.css'
-// import '@unocss/reset/tailwind.css'
+import '@unocss/reset/tailwind-compat.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/es/components/message/style/css'
 
 createApp(App).mount('#app')
--- blog-admin/vite.config.js
+++ blog-admin/vite.config.js
@@ -2,15 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import ElementPlus from 'unplugin-element-plus/vite'
 import UnoCSS from 'unocss/vite'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS()
+    UnoCSS(),
+    // ElementPlus({
+    //   // options
+    // })
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
