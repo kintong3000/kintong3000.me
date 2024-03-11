4348504b2e6a2e3513a86e36b0addb08591eeab5
Kintong Cheung <cheungkintong3000@gmail.com>
2024-03-12 01:35:24 +0800

    test

--- blog-fronted/config/fetchConfig.ts
+++ blog-fronted/config/fetchConfig.ts
@@ -2,11 +2,12 @@ const isProd:boolean = process.env.NODE_ENV === 'production'
 
 const devUrl:string = 'http://127.0.0.1:8080'
 
-const runtimeconfig = useRuntimeConfig()
-
-console.log(runtimeconfig.public.API_URL);
-// @ts-ignore
-const prodUrl:string = runtimeconfig.public.API_URL;
+// const runtimeconfig = useRuntimeConfig()
+//
+// console.log(runtimeconfig.public.API_URL);
+// // @ts-ignore
+// const prodUrl:string = runtimeconfig.public.API_URL;
+const prodUrl:string = "test";
 
 const config:{isProd:boolean,api:string} = {
     isProd,
