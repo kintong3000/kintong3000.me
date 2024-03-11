ced1a4a4826214a3d9b4ac2f19a3122a12be0591
Kintong Cheung <cheungkintong3000@gmail.com>
2024-03-11 23:13:10 +0800

    test

--- blog-fronted/config/fetchConfig.ts
+++ blog-fronted/config/fetchConfig.ts
@@ -1,7 +1,7 @@
 const isProd:boolean = process.env.NODE_ENV === 'production'
 
 const devUrl:string = 'http://127.0.0.1:8080'
-
+console.log(process.env.API_URL);
 // @ts-ignore
 const prodUrl:string = process.env.API_URL;
 
